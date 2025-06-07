543f4765a73bcbdac9ffaea7ac3ccfb9d0d7cd03
Carr Street <k.shepovalov@gmail.com>
2025-06-07 19:46:25 +0700

    I formatted the code in revseq.c

--- src/Task_02_Arrays/revseq.c
+++ src/Task_02_Arrays/revseq.c
@@ -2,64 +2,59 @@
 #include <stdlib.h>
 
 typedef struct Node {
-    int number;
-    struct Node *prev;
-    struct Node *next;
+  int number;
+  struct Node *prev;
+  struct Node *next;
 } Node;
 
 int main() {
-    Node *head = NULL;
-    Node *tail = NULL;
-    int num;
-    char c;
+  Node *head = NULL;
+  Node *tail = NULL;
+  int num;
+  char c;
 
-    printf("Введите числа через пробел: ");
+  printf("Введите числа через пробел: ");
 
-    // Считываем числа до конца строки
-    while (scanf("%d", &num) == 1) {
-        // Создаём новый узел
-        Node *new_node = (Node*)malloc(sizeof(Node));
-        if (new_node == NULL) {
-            printf("Ошибка выделения памяти\n");
-            exit(1);
-        }
-        
-        new_node->number = num;
-        new_node->prev = tail;
-        new_node->next = NULL;
-
-        if (head == NULL) {
-            head = new_node;
-        } else {
-            tail->next = new_node;
-        }
-        tail = new_node;
-
-        // Проверяем следующий символ
-        c = getchar();
-        if (c == '\n') break;
-        if (c != ' ') {
-            printf("Ошибка: числа должны разделяться пробелами\n");
-            exit(1);
-        }
+  while (scanf("%d", &num) == 1) {
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL) {
+      printf("Ошибка выделения памяти\n");
+      exit(1);
     }
 
-    // Выводим числа в обратном порядке
-    printf("Массив в обратном порядке: ");
-    Node *current = tail;
-    while (current != NULL) {
-        printf("%d ", current->number);
-        current = current->prev;
-    }
-    printf("\n");
+    new_node->number = num;
+    new_node->prev = tail;
+    new_node->next = NULL;
 
-    // Освобождаем память
-    current = head;
-    while (current != NULL) {
-        Node *temp = current;
-        current = current->next;
-        free(temp);
+    if (head == NULL) {
+      head = new_node;
+    } else {
+      tail->next = new_node;
     }
+    tail = new_node;
 
-    return 0;
+    c = getchar();
+    if (c == '\n') break;
+    if (c != ' ') {
+      printf("Ошибка: числа должны разделяться пробелами\n");
+      exit(1);
+    }
+  }
+
+  printf("Массив в обратном порядке: ");
+  Node *current = tail;
+  while (current != NULL) {
+    printf("%d ", current->number);
+    current = current->prev;
+  }
+  printf("\n");
+
+  current = head;
+  while (current != NULL) {
+    Node *temp = current;
+    current = current->next;
+    free(temp);
+  }
+
+  return 0;
 }
