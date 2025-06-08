d53c10d47c3234f4dddc8c3ccff5ced8de82561e
Carr Street <k.shepovalov@gmail.com>
2025-06-08 23:04:08 +0700

    I nearly completed the 3rd task!

---
+++ src/Task_03_Pointers/the_fourth.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 256
+
+int main() {
+    char str[MAX_SIZE];
+    char substr[MAX_SIZE];
+    char *ptr = NULL;
+
+    printf("Введите строку: ");
+    fgets(str, MAX_SIZE, stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("Введите подстроку: ");
+    fgets(substr, MAX_SIZE, stdin);
+    substr[strcspn(substr, "\n")] = '\0';
+
+    ptr = strstr(str, substr);
+
+    if (ptr != NULL) {
+        printf("Адрес подстроки: %p\n", (void*)ptr);
+        printf("Остаток строки с найденной подстроки: : %s\n", ptr);
+    } else {
+        printf("Подстрока не найдена (NULL)\n");
+    }
+
+    return 0;
+}
