02db92e4e8096fb12502d1df4323388166c7f3e0
Carr Street <k.shepovalov@gmail.com>
2025-06-08 23:04:50 +0700

    I formatted the code

--- src/Task_03_Pointers/the_fourth.c
+++ src/Task_03_Pointers/the_fourth.c
@@ -4,26 +4,26 @@
 #define MAX_SIZE 256
 
 int main() {
-    char str[MAX_SIZE];
-    char substr[MAX_SIZE];
-    char *ptr = NULL;
+  char str[MAX_SIZE];
+  char substr[MAX_SIZE];
+  char *ptr = NULL;
 
-    printf("Введите строку: ");
-    fgets(str, MAX_SIZE, stdin);
-    str[strcspn(str, "\n")] = '\0';
+  printf("Введите строку: ");
+  fgets(str, MAX_SIZE, stdin);
+  str[strcspn(str, "\n")] = '\0';
 
-    printf("Введите подстроку: ");
-    fgets(substr, MAX_SIZE, stdin);
-    substr[strcspn(substr, "\n")] = '\0';
+  printf("Введите подстроку: ");
+  fgets(substr, MAX_SIZE, stdin);
+  substr[strcspn(substr, "\n")] = '\0';
 
-    ptr = strstr(str, substr);
+  ptr = strstr(str, substr);
 
-    if (ptr != NULL) {
-        printf("Адрес подстроки: %p\n", (void*)ptr);
-        printf("Остаток строки с найденной подстроки: : %s\n", ptr);
-    } else {
-        printf("Подстрока не найдена (NULL)\n");
-    }
+  if (ptr != NULL) {
+    printf("Адрес подстроки: %p\n", (void *)ptr);
+    printf("Остаток строки с найденной подстроки: : %s\n", ptr);
+  } else {
+    printf("Подстрока не найдена (NULL)\n");
+  }
 
-    return 0;
+  return 0;
 }
--- src/Task_03_Pointers/the_third.c
+++ src/Task_03_Pointers/the_third.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
-    for(int *ptr=arr; ptr < (arr+9); ptr++) {
-        printf("%d ", *ptr);
-    }
-    printf("%d\n", *(arr+9));
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  for (int *ptr = arr; ptr < (arr + 9); ptr++) {
+    printf("%d ", *ptr);
+  }
+  printf("%d\n", *(arr + 9));
 }
