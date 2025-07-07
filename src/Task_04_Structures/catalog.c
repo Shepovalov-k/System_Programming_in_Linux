1d4ce79ee7828611d65d71cdb9d650817cd7bf82
Carr Street <k.shepovalov@gmail.com>
2025-07-07 15:08:08 +0700

    I added structures

--- src/Task_04_Structures/catalog.c
+++ src/Task_04_Structures/catalog.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+#define MENU                                                                                                         \
+  "1) Добавить абонента\n2) Удалить абонента\n3) Поиск абонентов по " \
+  "имени\n4) Вывод всех записей\n5) Выход"
+
+struct abonent {
+  char name[10];
+  char second_name[10];
+  char tel[10];
+};
+
+struct catalog
+{
+  struct abonent abonent_list[100];
+  unsigned __int128 state;
+};
+
+
+void work_loop(struct catalog* my_catalog) { printf(MENU); }
 
 int main() {
-    return 0;
+  struct catalog my_catalog = {0};
+  work_loop(&my_catalog);
+  return 0;
 }
